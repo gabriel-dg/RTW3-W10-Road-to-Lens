4c9db694b16d35aaa674c9d21a71d3060484d2c8
gabriel-dg <gabrieldg@gmail.com>
2022-07-19 17:42:36 -0300

    css styling

--- components/Post.js
+++ components/Post.js
@@ -1,17 +1,55 @@
+import { Card, Spacer, Avatar, User, Grid, Text, Row, Col, Button, Divider } from '@nextui-org/react';
+import { Container } from 'postcss';
+
 export default function Post(props) {
-    const post = props.post;
-  
-    return (
-      <div className="p-8">
-        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-          <div className="md:flex">
-            <div className="p-8">
-              <p className="mt-2 text-xs text-slate-500 whitespace-pre-line">
-                {post.metadata.content}
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+  const post = props.post;
+
+  return (
+
+
+    <Grid.Container class="place-items-center">
+
+      <Grid>
+        <Card css={{ p: "$10", mw: "500px", boxShadow: '0 0 0 3px blueviolet', paddingTop: 20, margin: 20, }}>
+
+          <Card.Header>
+            <Text
+              size={12}
+              weight="bold"
+              color="#FFA07A"
+            >
+              Date: {post.createdAt}
+            </Text>
+          </Card.Header>
+          <Divider />
+          <Card.Body>
+            <Text
+              size={14}
+              css={{
+                textGradient: "45deg, $blue600 -20%, $pink600 50%",
+              }}
+              weight="semibold"
+            >
+              {post.metadata.content}
+            </Text>
+          </Card.Body>
+          <Divider />
+          <Card.Footer>
+            <Row>
+              <Col>
+                <Text size={12} color="#7CFC00">
+                  Upvotes: {post.stats.totalUpvotes}
+                </Text>
+              </Col>
+              <Col>
+                <Text size={12} color="#7CFC00">
+                  Downvotes: {post.stats.totalDownvotes}
+                </Text>
+              </Col>
+            </Row>
+          </Card.Footer>
+        </Card>
+      </Grid>
+    </Grid.Container>
+  );
+}
